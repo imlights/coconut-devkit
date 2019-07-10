da2b14e7aa7b077f0cfe920add9776931f188340
Trent Charles <trentacharles@gmail.com>
2019-07-10 11:41:42 -0400

    Fix autoupdate

--- update.js
+++ update.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const child_process = require("child_process");
+
 console.log("Checking for Updates...");
 
 UpdateMain();
@@ -7,8 +9,14 @@ UpdateMain();
 function run(cmd) {
     return new Promise((resolve, reject) => {
         child_process.exec(cmd, (err, stdout, stderr) => {
-            resolve([err && err.code || 0, stdout, stderr]);
+            if (err)
+                reject([err && err.code || 0, stdout, stderr]);
+            else
+                resolve([err && err.code || 0, stdout, stderr]);
         });
+    }).catch(data => {
+        console.error(data[0]);
+        return data;
     });
 }
 
@@ -17,7 +25,7 @@ function TryUpdate() {
         let [c, out, err] = await run("git fetch");
         if (c) {
             console.error("Crash while fetching update from Repo.");
-            return resolve(false);
+            return reject(c);
         }
         if (!out && !err) {
             console.log("No updates to fetch from Repo.");
@@ -27,21 +35,30 @@ function TryUpdate() {
         [c, out, err] = await run("git pull origin master");
         if (c || err) {
             console.error("Crash while pulling new source files");
-            return resolve(false);
+            return reject(c);
         } else {
             return resolve(true);
         }
 
-    }).catch((err) => console.error);
+    }).catch((err) => {
+        console.error("err: " + err);
+        return false;
+    });
 }
 
 async function UpdateMain() {
-    let returnValue = await TryUpdate();
-    if (returnValue) {
-        console.log("Autoupdate successfully exited, launching...");
-        require("electron-main");
-    } else {
-        console.error("Autoupdate closed with an error, Unable to launch.");
+    try {
+        let returnValue = await TryUpdate();
+        if (returnValue) {
+            console.log("Autoupdate successfully exited, launching...");
+            require("./electron-main");
+        } else {
+            console.error("Autoupdate closed with an error, Unable to launch.");
+            process.exit();
+        }
+    } catch (e) {
+        console.error("Fatal Crash: Unable to launch devkit.");
+        console.error(e);
         process.exit();
     }
 }
